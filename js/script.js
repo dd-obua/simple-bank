3bcd9f8fb501e37ee37f71ec1bb0c8b76cdfa75a
Obua Denis Daniel <dd.obua@gmail.com>
2023-10-20 15:44:37 +0300

    Make movements a list

--- js/script.js
+++ js/script.js
@@ -66,14 +66,14 @@ const displayMovements = (movements) => {
   movements.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
-      <div class="movements__row">
+      <li class="movements__row">
         <div class="movements__type movements__type--${type}">
           ${i + 1} ${type}
         </div>
         <div class="movements__value">
           ${mov}
         </div>
-      </div>
+      </li>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
